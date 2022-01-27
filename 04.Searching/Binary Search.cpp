1c579efb7341d5fd6d4b29c4667a178e706ad776
Pankaj <chaudharypankajatt0001@gmail.com>
2022-01-27 22:39:26 +0530

    .

---
+++ 04.Searching/Binary Search.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+void scan(int A[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cin >> A[i];
+    }
+}
+void print(int A[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << A[i];
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    int x;
+    cin >> x;
+    scan(a, n);
+    bool flag = false;
+    int low = 0, high = n - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (a[mid] == x)
+        {
+            cout << mid;
+            flag = true;
+            break;
+        }
+        else if (a[mid] > x)
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    if (!flag)
+    {
+        cout << -1;
+    }
+
+    return 0;
+}
