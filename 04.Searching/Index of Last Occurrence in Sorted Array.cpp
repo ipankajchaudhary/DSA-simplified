54192ab1e54952e8fea5d21f354954f99aff1bbb
Pankaj <chaudharypankajatt0001@gmail.com>
2022-01-27 22:57:18 +0530

    .

---
+++ 04.Searching/Index of Last Occurrence in Sorted Array.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void scan(int A[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cin >> A[i];
+    }
+}
+void print(int A[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << A[i];
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    int x;
+    cin >> x;
+    scan(a, n);
+    bool flag = false;
+    int low = 0, high = n - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (a[mid] > x)
+        {
+            high = mid - 1;
+        }
+        else if (a[mid] < x)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+           if(mid == n-1 || a[mid]!=a[mid+1])
+           {
+               cout << mid;
+               flag = true;
+               break;
+           }
+           else{
+                low = mid +1;
+           }
+        }
+    }
+    if (!flag)
+    {
+        cout << -1;
+    }
+
+    return 0;
+}
