2a173e650d6d092e69a206186851b40d14869f94
Pankaj <chaudharypankajatt0001@gmail.com>
2022-02-03 22:26:57 +0530

    .

---
+++ 04.Searching/Koko Eating Bananas.cpp
@@ -0,0 +1,86 @@
+// Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+
+// Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+
+// Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+
+// Return the minimum integer k such that she can eat all the bananas within h hours.
+
+// Example 1:
+// Input: piles = [3,6,7,11], h = 8
+// Output: 4
+
+// Example 2:
+// Input: piles = [30,11,23,4,20], h = 5
+// Output: 30
+
+// Example 3:
+// Input: piles = [30,11,23,4,20], h = 6
+// Output: 23
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void scan(int A[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cin >> A[i];
+    }
+}
+void print(int A[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << A[i];
+    }
+}
+bool ispossible(vector<int> p, int mid, int h)
+{
+    int count = 0;
+    for (int i = 0; i < p.size(); i++)
+    {
+        count += (p[i] / mid);
+        if (p[i] % mid != 0)
+            count += 1;
+    }
+    return (count <= h);
+}
+int minEatingSpeed(vector<int> &p, int h)
+{
+    int low = 1;
+    int high = *max_element(p.begin(), p.end());
+    int ans;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (ispossible(p, mid, h))
+        {
+            high = mid - 1;
+            ans = mid;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+int main()
+{
+    vector<int> p;
+    int n;
+    cin >> n;
+    int h;
+    cin >> h;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        p.push_back(x);
+    }
+
+    cout << minEatingSpeed(p, h);
+
+    return 0;
+}
