45ed7c4bb168e32e83d77483aeb6e9d1b6a0c88c
Pankaj <chaudharypankajatt0001@gmail.com>
2022-02-03 22:31:38 +0530

    .

---
+++ 04.Searching/Find in Mountain Array.cpp
@@ -0,0 +1,114 @@
+// You may recall that an array arr is a mountain array if and only if:
+
+// arr.length >= 3
+// There exists some i with 0 < i < arr.length - 1 such that:
+// arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+// arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+// Given a mountain array mountainArr, return the minimum index such that mountainArr.get(index) == target. If such an index does not exist, return -1.
+
+// You cannot access the mountain array directly. You may only access the array using a MountainArray interface:
+
+// MountainArray.get(k) returns the element of the array at index k (0-indexed).
+// MountainArray.length() returns the length of the array.
+// Submissions making more than 100 calls to MountainArray.get will be judged Wrong Answer. Also, any solutions that attempt to circumvent the judge will result in disqualification.
+
+// Example 1:
+
+// Input: array = [1,2,3,4,5,3,1], target = 3
+// Output: 2
+// Explanation: 3 exists in the array, at index=2 and index=5. Return the minimum index, which is 2.
+// Example 2:
+
+// Input: array = [0,1,2,4,2,1], target = 3
+// Output: -1
+// Explanation: 3 does not exist in the array, so we return -1.
+
+
+int findInMountainArray(int target, MountainArray &mountainArr)
+{
+    bool flag = false;
+    int n = mountainArr.length();
+    int low = 0, high = n - 1;
+    int k;
+    int peak;
+    while (low <= high)
+    {
+        int k = (low + high) / 2;
+        if ((k == 0 || mountainArr.get(k) >= mountainArr.get(k - 1)) && ((k == n - 1 || mountainArr.get(k) >= mountainArr.get(k + 1))))
+        {
+            peak = k;
+            flag = true;
+            break;
+        }
+        if (k > 0 && (mountainArr.get(k) <= mountainArr.get(k - 1)))
+        {
+            high = k - 1;
+        }
+        else
+        {
+            low = k + 1;
+        }
+    }
+    if (!flag)
+    {
+        peak = k - 1;
+    }
+    bool first = false;
+    low = 0;
+    high = peak;
+    int ans;
+    while (low <= high)
+    {
+
+        int k = (low + high) / 2;
+        int curr = mountainArr.get(k);
+        if (curr == target)
+        {
+            ans = k;
+            first = true;
+            return ans;
+
+            break;
+        }
+        if (curr > target)
+        {
+            high = k - 1;
+        }
+        else
+        {
+            low = k + 1;
+        }
+    }
+    if (!first)
+    {
+        low = peak + 1;
+        high = n - 1;
+        while (low <= high)
+        {
+            int k = (low + high) / 2;
+            int curr = mountainArr.get(k);
+            if (curr == target)
+            {
+                ans = k;
+                first = true;
+                return ans;
+
+                break;
+            }
+            if (curr > target)
+            {
+                low = k + 1;
+            }
+            else
+            {
+                high = k - 1;
+            }
+        }
+    }
+    if (!first)
+    {
+        return -1;
+    }
+
+    return ans;
+}
