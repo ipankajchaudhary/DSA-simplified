a992412f3b39de99bf1c58f69f4d2c0b9bb9b43b
Pankaj <chaudharypankajatt0001@gmail.com>
2022-02-06 19:01:23 +0530

    .

--- 05.Sorting/Sort array of three types of element.cpp
+++ 05.Sorting/Sort array of three types of element.cpp
@@ -12,11 +12,36 @@ void print(int A[], int n)
 {
     for (int i = 0; i < n; i++)
     {
-        cout << A[i];
+        cout << A[i] << " ";
     }
+}
+void sort012(int a[], int arr_size)
+{
+	int lo = 0;
+	int hi = arr_size - 1;
+	int mid = 0;
+
+	while (mid <= hi) {
+		switch (a[mid]) {
+		case 0:
+			swap(a[lo++], a[mid++]);
+			break;
+		case 1:
+			mid++;
+			break;
+		case 2:
+			swap(a[mid], a[hi--]);
+			break;
+		}
+	}
 }
 int main()
 {
-
+     int n;
+    cin >> n;
+    int arr[n];
+    scan(arr, n);
+    sort012(arr, n);
+    print(arr, n);
     return 0;
 }
