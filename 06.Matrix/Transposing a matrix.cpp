a6248f12458b0a724aa1233371e2b15fea595b0e
Pankaj <chaudharypankajatt0001@gmail.com>
2022-02-07 23:39:24 +0530

    .

---
+++ 06.Matrix/Transposing a matrix.cpp
@@ -0,0 +1,11 @@
+void tranpose(vector<vector<int>> &matrix)
+{
+    int n = matrix.size();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+}
