314ad5f6feaaefbdbc0aee36606ef1349a693bc2
Pankaj <chaudharypankajatt0001@gmail.com>
2022-02-17 16:55:45 +0530

    .

---
+++ 09.LinkedLists/Deletion from a Circular Linked List.cpp
@@ -0,0 +1,46 @@
+// Delete head of CLL 
+
+Node *delHead(Node *head)
+{
+    if(head == NULL)
+    {
+        return NULL;
+    }
+    if(head->next == head)
+    {
+        delete head;
+        return NULL;
+    }
+    else{
+        head->data = head->next->data;
+        Node *temp = head->next;
+        head->next = head->next->next;
+        delete temp;
+        return head;
+    }
+}
+
+// Delete kth from cll 
+
+Node *delkth(Node *head, int k)
+{
+    if(!head)
+    {
+        return NULL;
+    }
+    if(k == 1)
+    {
+        delHead(head);
+    }
+    else{
+        Node *curr = head;
+        for (int i = 0; i < k - 2;i++)
+        {
+            curr = curr->next;
+        }
+        Node *temp = curr->next;
+        curr->next = curr->next->next;
+        delete temp;
+    }
+    return head;
+}
