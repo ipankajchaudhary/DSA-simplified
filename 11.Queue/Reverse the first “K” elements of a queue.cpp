f2b1b7c8e19b9903e7956c595f9b6fca8ef9789a
Pankaj Chaudhary <chaudharypankajatt0001@gmail.com>
2022-03-09 17:24:57 +0530

    .

---
+++ 11.Queue/Reverse the first “K” elements of a queue.cpp
@@ -0,0 +1,28 @@
+queue<int> modifyQueue(queue<int> q, int k) {
+    // add code here.
+    stack<int> s;
+    int n = q.size();
+    int x = n-k;
+    while(k--)
+    {
+        s.push(q.front());
+        q.pop();
+    }
+    while(!s.empty())
+    {
+        q.push(s.top());
+        s.pop();
+    }
+    if(n == k) return q;
+    else{
+        
+        
+        while(x>0)
+        {
+            q.push(q.front());
+            q.pop();
+            x--;
+        }
+        return q;
+    }
+}
