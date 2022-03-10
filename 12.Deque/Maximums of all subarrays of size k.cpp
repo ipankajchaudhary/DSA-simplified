5d7bb72aa842b878a12886799c610080bc2669e3
Pankaj <chaudharypankajatt0001@gmail.com>
2022-03-11 01:00:56 +0530

    .

---
+++ 12.Deque/Maximums of all subarrays of size k.cpp
@@ -0,0 +1,28 @@
+vector <int> max_of_subarrays(int *arr, int n, int k)
+    {
+        deque<int> dq;
+        for(int i = 0;i<k;i++)
+        {
+            while(!dq.empty() && arr[i] >= arr[dq.back()])
+            {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+        }
+        vector<int> v;
+        for(int i = k;i<n;i++)
+        {
+            v.push_back(arr[dq.front()]);
+            while(!dq.empty() && dq.front() <= i-k)
+            {
+                dq.pop_front();
+            }
+            while(!dq.empty() && arr[i] >= arr[dq.back()])
+            {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+        }
+        v.push_back(arr[dq.front()]);
+        return v;
+    }
