9c7ada7d6f672490784a079dca20683901aeeb2c
Pankaj <chaudharypankajatt0001@gmail.com>
2022-03-12 20:01:28 +0530

    .

---
+++ 11.Queue/First non repeating character in a stream;.cpp
@@ -0,0 +1,21 @@
+string FirstNonRepeating(string s)
+{
+
+    int n = s.size();
+    unordered_map<char, int> map;
+    string ans = "";
+    queue<char> q;
+    for (int i = 0; i < n; i++)
+    {
+        if (!map[s[i]])
+            q.push(s[i]);
+        map[s[i]]++;
+        while (!q.empty() && map[q.front()] > 1)
+            q.pop();
+        if (q.empty())
+            ans += "#";
+        else
+            ans += q.front();
+    }
+    return ans;
+}
