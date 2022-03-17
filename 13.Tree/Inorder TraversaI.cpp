6ea4abdc6c7278029137d1648870118ce2fc0b18
Pankaj <chaudharypankajatt0001@gmail.com>
2022-03-17 22:50:04 +0530

    .

--- 13.Tree/Inorder TraversaI.cpp
+++ 13.Tree/Inorder TraversaI.cpp
@@ -1,15 +1,37 @@
-void help(Node * root , vector<int>&ans)
+void help(Node *root, vector<int> &ans)
+{
+    if (root == NULL)
+        return;
+    help(root->left, ans);
+    ans.push_back(root->data);
+    help(root->right, ans);
+}
+
+vector<int> inOrder(Node *root)
+{
+    vector<int> ans;
+    help(root, ans);
+    return ans;
+}
+
+// Iterative
+
+vector<int> inOrder(Node *root)
+{
+    vector<int> v;
+    stack<Node *> st;
+    Node *curr = root;
+    while (curr != NULL || st.empty() == false)
     {
-        if(root == NULL)return ;
-        help(root->left , ans);
-        ans.push_back(root->data);
-        help(root->right,ans);
-        
+        while (curr != NULL)
+        {
+            st.push(curr);
+            curr = curr->left;
+        }
+        curr = st.top();
+        st.pop();
+        v.push_back(curr->data);
+        curr = curr->right;
     }
-    
-   
-    vector<int> inOrder(Node* root) {
-        vector<int>ans;
-        help(root,ans);
-        return ans;
-    }
+    return v;
+}
--- 13.Tree/Preorder Traversal.cpp
+++ 13.Tree/Preorder Traversal.cpp
@@ -1,14 +1,42 @@
-void help(Node * root , vector<int>&ans)
-    {
-        if(root == NULL)return ;
-        ans.push_back(root->data);
-        help(root->left , ans);
-        help(root->right,ans);
-        
-    }
-vector <int> preorder(Node* root)
+void help(Node *root, vector<int> &ans)
 {
-        vector<int>ans;
-        help(root,ans);
-        return ans;
+    if (root == NULL)
+        return;
+    ans.push_back(root->data);
+    help(root->left, ans);
+    help(root->right, ans);
+}
+vector<int> preorder(Node *root)
+{
+    vector<int> ans;
+    help(root, ans);
+    return ans;
+}
+
+// Iterative
+vector<int> preOrder(Node *root)
+{
+    vector<int> v;
+    if (!root)
+        return v;
+    stack<Node *> st;
+    Node *curr = root;
+    while (curr || !st.empty())
+    {
+        while (curr)
+        {
+            v.push_back(curr->data);
+            if (curr->right)
+            {
+                st.push(curr->right);
+            }
+            curr = curr->left;
+        }
+        if (!st.empty())
+        {
+            curr = st.top();
+            st.pop();
+        }
+    }
+    return v;
 }
