b6f13bd34c678d9adeeaed5e00f2a0a2ee863e2c
Pankaj Chaudhary <chaudharypankajatt0001@gmail.com>
2022-03-23 17:12:10 +0530

    .

---
+++ 14.Binary-Search-Tree/Find LCA of 2 nodes in a BST.cpp
@@ -0,0 +1,14 @@
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root){
+            return root;
+        }
+        if(root->val < p->val && root->val < q->val){
+            return lowestCommonAncestor(root->right,p,q);
+        }
+        else if(root->val > p->val && root->val > q->val){
+            return lowestCommonAncestor(root->left,p,q);
+        }
+        else{
+            return root;
+        }
+    }
