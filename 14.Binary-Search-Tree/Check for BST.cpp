2261a463de2efb0e955ff20a6f4cf481ac280ac2
Pankaj Chaudhary <chaudharypankajatt0001@gmail.com>
2022-03-23 08:40:48 +0530

    .

---
+++ 14.Binary-Search-Tree/Check for BST.cpp
@@ -0,0 +1,12 @@
+bool helper(TreeNode *root, long mn, long mx){
+        if(!root){
+            return true;
+        }
+        if((root->val <= mn) || (root->val >= mx)){
+            return false;
+        }
+        return helper(root->left,mn,root->val) && helper(root->right,root->val,mx);
+    }
+    bool isValidBST(TreeNode* root) {
+        return helper(root, LONG_MIN, LONG_MAX);
+    }
