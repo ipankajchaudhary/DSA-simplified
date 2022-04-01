140fe636d08b9e3a63a933bc2a98939530f7d8eb
Pankaj Chaudhary <chaudharypankajatt0001@gmail.com>
2022-04-01 07:47:00 +0530

    .

--- 16.Graph/detect cycle in a directed graph.cpp
+++ 16.Graph/detect cycle in a directed graph.cpp
@@ -1,1 +1,28 @@
-
+bool dfs(vector<int>adj[],int s, vector<bool> visited, vector<bool>recst){
+        visited[s] = true;
+        recst[s] = true;
+        for(auto u:adj[s]){
+            if(visited[u] == false && dfs(adj,u,visited,recst)){
+                return true;
+            }
+            else{
+                if(recst[u] == true){
+                    return true;
+                }
+            }
+        }
+        recst[s] = false;
+        return false;
+    }
+    bool isCyclic(int V, vector<int> adj[]) {
+        vector<bool> visited(V,false);
+        vector<bool> recst(V,false);
+        for(int i = 0;i<V;i++){
+            if(visited[i] == false){
+                if(dfs(adj,i,visited,recst)){
+                    return true;
+                }            
+            }
+        }
+        return false;
+    }
